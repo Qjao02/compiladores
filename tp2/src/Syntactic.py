5989d89371e5cc281f04dab5758b48615eb74d45
Joao Augusto da Cruz <joao_cruz02@hotmail.com>
2018-05-18 16:46:27 -0300

    adcionado algumas regras da gramatica

--- tp2/src/Syntactic.py
+++ tp2/src/Syntactic.py
@@ -16,6 +16,7 @@ class Syntactic():
     token = ''
     arrayToken = []
     indexToken = ''
+    ASA = ''
     
 
     def __init__ (self, arrayToken):
@@ -44,13 +45,165 @@ class Syntactic():
 
 
     def program(self):
-        for l in self.arrayToken:
-            print(l.getCodigoToken())
+       
+        #match first token for any code in c-small
         self.match('INT')
         self.match('MAIN')
         self.match('LBRACKET')
         self.match('RBRACKET')
         self.match('LBRACE')
+
+        #start recursion and build ASA
+        self.decl_comand()
+
+
+    
+
+    def decl_comand(self):
+        
+        if (self.token == 'INT' OR self.token == 'FLOAT'):
+            
+            self.type()
+            self.match('ID')
+            
+            if(self.token == 'COMMA' or self.token == 'PCOMMA' or self.token == 'ATTR'):
+                self.declaration2()
+
+
+        elif(self.token == 'LBRACE' OR self.token == 'ID' or self.token == )
+        
+    
+
+
+    def types(self):
+        
+        if(self.token == 'INT'):
+            self.match(self.token)
+        
+        elif(self.token == 'FLOAT'):
+            self.match('FLOAT')
+
+
+    def declaration(self)
+        pass
+
+    def declaration2(self):
+        if (self.token == 'COMMA'):
+            self.match('COMMA')
+            self.match('ID')
+            if(self.token == 'COMMA' or self.token == 'PCOMMA' or self.token == 'ATTR'):
+                self.declaration2()
+        
+        elif(self.token == 'PCOMMA'):
+            self.match('PCOMMA')
+
+        elif(self.token == 'ATTR):
+            self.match('ATTR')
+            if (self.token == 'ID' or self.token == 'INTEGER_CONST' or self.token == 'FLOAT_CONST' or self.token == 'LBRACKET'):
+                self.expression()
+            if(self.token == 'COMMA' or self.token == 'PCOMMA' or self.token == 'ATTR'):
+                self.declaration2()    
+
+       
+
+    def comand(self):
+        pass
+    
+    def block(self):
+        pass
+
+    def assignmentt(self):
+        pass
+
+    def comand_if(self):
+        pass
+    
+    def comand_else(self):
+        pass
+
+    def comand_when(self):
+        pass
+    
+    def comand_read(self):
+        pass
+    
+    def comand_print(self):
+        pass
+
+    def comand_for(self):
+        pass
+    
+    def att_for(self):
+        pass
+
+    def expression(self):
+        if (self.token == 'ID' or self.token == 'INTEGER_CONST' or self.token == 'FLOAT_CONST' or self.token == 'LBRACKET'):
+
+
+    
+    def expressaoOpc(self):
+        pass
+    
+    def conjunction(self):
+        pass
+    
+    def conjuction_opc(self):
+        pass
+
+    def equal(self):
+        if (self.token == 'ID' or self.token == 'INTEGER_CONST' or self.token == 'FLOAT_CONST' or self.token == 'LBRACKET'):
+            self.relacao()
+        
+    
+    def relation(self):
+        if (self.token == 'ID' or self.token == 'INTEGER_CONST' or self.token == 'FLOAT_CONST' or self.token == 'LBRACKET'):
+            self.add()
+    def op_equal(self):
+        pass
+
+    def relac_opc(self):
+        pass
+    
+    def op_rel(self):
+        pass
+    
+    def add(self):
+        if (self.token == 'ID' or self.token == 'INTEGER_CONST' or self.token == 'FLOAT_CONST' or self.token == 'LBRACKET'):
+            self.term()
+    
+    def add_opc(self):
+        pass
+    
+    def op_add(self):
+        pass
+
+    def term(self):
+        if (self.token == 'ID' or self.token == 'INTEGER_CONST' or self.token == 'FLOAT_CONST' or self.token == 'LBRACKET'):
+            self.fact()
+    
+    def term_opc(self):
+        pass
+    
+    def op_mult(self):
+        pass
+    
+    def fact(self):
+        if (self.token == 'ID'):
+            self.match('ID')
+        
+        elif(self.token == 'INTEGER_CONST'):
+            self.match('INTEGER_CONST')
+        
+        elif(self.token == 'FLOAT_CONST'):
+            self.match('FLOAT_CONST')
+
+        elif(self.token == 'LBRACKET'):
+            self.match('LBRACKET')
+            if (self.token == 'ID' or self.token == 'INTEGER_CONST' or self.token == 'FLOAT_CONST' or self.token == 'LBRACKET'):
+                self.expression()
+            self.match('RBRACKET')
+
+    
 
 
     '''
